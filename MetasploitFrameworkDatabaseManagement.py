58c0dec69c2577a54efa3f6b52ba91f3259ef113
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-05-12 06:41:17 -0400

    Add files via upload

---
+++ MetasploitFrameworkDatabaseManagement.py
@@ -0,0 +1,57 @@
+#!/usr/bin/eny python
+# -*- coding:utf-8 -*-
+
+import os
+
+MetasploitFramework_databasemanagement_ico = """
+####################################################################################
+#         PYTHON - METASPLOIT FRAMEWORK DATABASE MANAGEMENT - GH0ST S0FTWARE       #
+####################################################################################
+#                                    CONTACT                                       #
+####################################################################################
+#                            DEVELOPER : İSMAİL TAŞDELEN                           #                       
+#                      Mail Address : pentestdatabase@gmail.com                    #
+#               LINKEDIN : https://www.linkedin.com/in/ismailtasdelen              #
+#                           Whatsapp : + 90 534 295 94 31                          #
+####################################################################################
+"""
+
+print MetasploitFramework_databasemanagement_ico
+
+def msfdb_init():
+	os.system("msfdb init")
+
+def msfdb_reinit():
+	os.system("msfdb reinit")
+
+def msfdb_delete():
+	os.system("msfdb delete")
+
+def msfdb_start():
+	os.system("msfdb start")
+
+def msfdb_stop():
+	os.system("msfdb stop")
+
+islemler_ico = """
+(1) Metasploit-Framework Database Init
+(2) Metasploit-Framework Database Reinit
+(3) Metasploit-Framework Database Delete
+(4) Metasploit-Framework Database Start
+(5) Metasploit-Framework Database Stop
+"""
+
+print islemler_ico
+
+islem = input("Yapılcak işlem numarasını giriniz : ")
+
+if islem == 1:
+	msfdb_init()
+if islem == 2:
+	msfdb_reinit()
+if islem == 3:
+	msfdb_delete()
+if islem == 4:
+	msfdb_start()
+if islem == 5:
+	msfdb_stop()
